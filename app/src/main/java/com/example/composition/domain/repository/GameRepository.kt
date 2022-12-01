2cae37cef56080b5ffc0d2ba76e1683b8cd87320
Yoru <yorucof@gmail.com>
2022-12-01 17:22:53 +0300

    Preparation for work. Added data, domain, presentation packages. Added layout to resource

---
+++ app/src/main/java/com/example/composition/domain/repository/GameRepository.kt
@@ -0,0 +1,19 @@
+package com.example.composition.domain.repository
+
+import com.example.composition.domain.entity.GameResult
+import com.example.composition.domain.entity.GameSettings
+import com.example.composition.domain.entity.Level
+import com.example.composition.domain.entity.Question
+
+interface GameRepository {
+
+    fun getGameSettings(level: Level): GameSettings
+
+    fun generateQuestion(
+        maxSumValue: Int,
+        countOfOptions: Int
+    ): Question
+
+    fun getGameResult(): GameResult
+
+}
